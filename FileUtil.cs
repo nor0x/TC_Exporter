19319beeef24986d57657a4722c6a5106bb2f112
nor0x <nor0x@users.noreply.github.com>
2024-06-07 11:06:58 +0200

    net8 bump

--- FileUtil.cs
+++ FileUtil.cs
@@ -2,16 +2,15 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.JSInterop;
 
-namespace TC_Export
+namespace TC_Export;
+
+public static class FileUtil
 {
-    public static class FileUtil
+    public async static Task SaveAs(IJSRuntime js, string filename, byte[] data)
     {
-        public async static Task SaveAs(IJSRuntime js, string filename, byte[] data)
-        {
-            await js.InvokeAsync<object>(
-                "saveAsFile",
-                filename,
-                Convert.ToBase64String(data));
-        }            
-    }
+        await js.InvokeAsync<object>(
+            "saveAsFile",
+            filename,
+            Convert.ToBase64String(data));
+    }            
 }
--- Program.cs
+++ Program.cs
@@ -1,23 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Text;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.JSInterop;
-using System.Net.Http;
+using TC_Export;
 
-namespace TC_Export
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            await builder.Build().RunAsync();
-        }
-    }
-}
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
